cbd8d10f789dfef6568c8a492f3e8f641c3374b9
bdrenn <briansdrennan@gmail.com>
2020-03-31 15:49:43 -0700

    filing work

--- src/fileme.py
+++ src/fileme.py
@@ -5,24 +5,10 @@ import time
 ip = '127.0.0.1'
 
 class Client:
-    def __init__(self):
-        self.connect()
+    def __init__(self, sock):
+        self.connect(sock)
 
-    def connect(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        port = 9999
-
-        try: 
-            sock.bind(('',port)) # '' indicates that the server will listen to other computers
-            sock.listen(1)
-            print("System has started!")
-            while True:
-                client,addr = sock.accept()
-                print("Peer added %s " %  str(addr[1]))
-        except:
-            print("Adding new client")
-
-        # New Peer
+    def connect(self, sock):
         sock.connect((ip,port))
         self.runner()
 
@@ -31,9 +17,24 @@ class Client:
             time.sleep(2)
             print("peer active")
 
-# Runner
-peer = Client()
+# Network
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 9999
+peers = dict()
+user_file = input("Please enter a file: ")
 
+try:
+    sock.bind(('',port))
+    sock.listen(1)
+    print("System has started")
+    while True:
+        print(peers)
+        client, addr = sock.accept()
+        peers[user_file] = addr[1]
+        print("Peer added %s " % str(addr[1]))
+except:
+    print("Network active... adding new client")
 
+Client(sock)
 
 
