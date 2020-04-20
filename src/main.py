76b16ac7bda5b155a036552011c5928f629569a2
Jackson Bursch <jacksonbursch@Jacksons-MacBook-Pro.local>
2020-04-20 12:31:16 -0700

    lots of functionality

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
 import sys
 import os
+from node import Node
 
+def decide_first_node(my_port, addr=None, network_port=None, y=None):
+	node = Node(my_port)
+	if y == 'y':
+		node.first_node()
+	try:
+		node.bootstrap_node(addr, network_port)
+		print("I'm bootstrapping")
+	except Exception as e:
+		print(e)
+	return node
+	
 
 print("-----------------------------------------------------------------")
 print("**************************** Welcome ****************************")
@@ -16,17 +28,18 @@ if ans[0] == "Y" or ans[0] == "y":
 
 	## EXAMPLES FOR HOW WE CAN GET USER INPUT FROM CLT ##
 
-	#print("\nExample for entering network - 0, 123.123.123, 4586")
-	#nodeInput = input("\nPlease input your PeerID, IP Address, and Port : ")
-	#[peerid, host, port] = nodeInput.split(",")
-	#print(peerid,host,port)
-	#os.system('python3 node.py ' + str(peerid) + ' ' + str(host) + ' ' + str(port))
-
-	## CALL THE FILES ##
-
-	### MIGHT NEED TO CHECK IF FIRST NODE ### also might be calling function not file
-	#os.system('python3 first_node.py')
-
+	print("\nExample for entering network - 123.123.123.123, 4586, 3453")
+	ask = input("\nAre you the first node? (Yes/No) : ")
+	if ask[0] == 'y' or ask[0] == 'Y':
+		nodeInput = input("\nPlease input your Port : ")
+		my_port = nodeInput
+		node = decide_first_node(my_port, y=ask[0])
+	else:
+		nodeInput = input("\nPlease input your IP Address, Port, Network Port : ")
+		[addr, my_port, network_port] = nodeInput.split(",")
+		node = decide_first_node(my_port, addr=addr, network_port=network_port)
+	
+	
 
 	## CONTINUOUS LOOP ASKING WHAT THE USER WANTS TO DO ##
 	flag = True
@@ -36,14 +49,15 @@ if ans[0] == "Y" or ans[0] == "y":
 		secondInput = input("\nThe options are get a file, set a file, or quit (get/set/quit) : ")
 		if secondInput[0] == 'S' or secondInput[0] == 's':
 			#This is where we would call our set function
+			node.set('hello','file')
 			print("\nSo you wanna SET a file?")
 			print("\nWould you like to do anything else?")
 		elif secondInput[0] == 'G' or secondInput[0] == 'g':
 			#This is where we could call our get function
+			print(node.get('hello'))
 			print("\nSo you wanna GET a file?")
 			print("\nWould you like to do anything else?")
 		elif secondInput[0] == 'Q' or secondInput[0] == 'q':
-			#This is where we could call our get function
 			print("\nThanks for joining, good bye!")
 			flag = False
 		else:
@@ -52,3 +66,6 @@ if ans[0] == "Y" or ans[0] == "y":
 else:
 	print("That's too bad! Maybe next time.")
 	sys.exit(0)
+
+		
+
--- src/node.py
+++ src/node.py
@@ -1,138 +1,34 @@
-import socket
-import traceback
-import threading
-import struct
+import logging
+import asyncio
+import sys
+from kademlia.network import Server
 
-class BTPeerConnection:
-    def __init__(self, peerid, host, port, sock=None):
-        self.id = peerid
-        
-        if not sock:
-            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.s.connect((host,int(port)))
-        else:
-            self.s = sock
-        
-        self.sd = self.s.makefile('rw', 0)
-
-    def recvdata(self):
-        
-        try:
-            msgtype = self.sd.read(4)
-            if not msgtype:
-                return(None, None)
-            lenstr = self.sd.read(4)
-            msglen = int(struct.unpack("!L", lenstr)[0])
-            msg = ""
-
-            while len(msg) != msglen:
-                data = self.sd.read(min(2048, msglen - len(msg)))
-                if not len(data):
-                    break
-                msg += data
-
-            if len(msg) != msglen:
-                return(None, None)
-        
-        except KeyboardInterrupt:
-            raise
-
-        return (msgtype, msg)
-        
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+log = logging.getLogger('kademlia')
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
 
 
 class Node:
-    def __init__(self, maxpeers, serverport, myid=None, serverhost = None):
-        self.maxpeers = int(maxpeers)
-        self.serverport = int(serverport)
-
-        # If not supplied, the host name/IP address will be determined
-        # by attempting to connect to an Internet host like Google.
-        if serverhost:
-            self.serverhost = serverhost
-        else: 
-            self.__initserverhost()
-
-        # If not supplied, the peer id will be composed of the host address
-        # and port number
-        if myid:
-            self.myid = myid
-        else:
-            self.myid = '%s:%d' % (self.serverhost, self.serverport)
-
-        # list (dictionary/hash table) of known peers
-        self.peers = {}  
-
-        # used to stop the main loop
-        self.shutdown = False  
-
-        self.handlers = {}
-        self.router = None
-
-    def makeserversocket(self, port, backlog=5):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(('', port))
-        s.listen(backlog)
-        return s
-
-    def __handlepeer(self, clientsock):
-        print('Connected' + str(clientsock.getpeername()))
-
-        host, port = clientsock.getpeername()
-        peerconn = BTPeerConnection(None, host, port, clientsock)
-
-        try:
-            msgtype, msgdata = peerconn.recvdata()
-            if msgtype:
-                msgtype = msgtype.upper()
-            if msgtype not in self.handlers:
-                print('Not handled: %s: %s' % (msgtype, msgdata))
-            else:
-                print('Handling peer msg: %s: %s' % (msgtype,msgdata))
-                self.handlers[msgtype](peerconn, msgdata)
-        except KeyboardInterrupt:
-            raise
-
-        print('Disconnecting ' + str(clientsock.getpeername()))
-
-    def __initserverhost(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("www.google.com",80))
-        self.serverhost = s.getsockname()[0]
-        s.close
-    
-    def mainloop(self):
-        s = self.makeserversocket(self.serverport)
-        print('Server started: %s (%s:%d)' % (self.myid, self.serverhost, self.serverport))
+    def __init__(self, port):
+        self.loop = asyncio.get_event_loop()
+        self.loop.set_debug(True)
+        self.server = Server()
+        self.loop.run_until_complete(self.server.listen(int(port)))
         
-        while not self.shutdown:
-            try:
-                print( 'Listening for connections...')
-                clientsock, clientaddr = s.accept()
-                t = threading.Thread(target = self.__handlepeer, args = [clientsock])
-                print("peer connected")
-                t.start()
-            except KeyboardInterrupt:
-                self.shutdown = True
-                continue
-
-        print('Main loop exiting')
-        s.close()
-
-
-
-# Main
-for i in range(0,10):
-    print(i)
-    try:
-        node1 = Node(0, 1000)
-        node1.mainloop()
-    except:
-        pass
+    def first_node(self):
+        self.loop.run_forever()
         
-    try:
-        print("network active")
-        node2 = Node(0, 1000, '192.168.1.2')
-    except:
-        print("can't connect to server")
+    def bootstrap_node(self, addr, port):
+        bootstrap_node = (addr, int(port))
+        self.loop.run_until_complete(self.server.bootstrap([bootstrap_node]))
+
+    def get(self, key):
+        loop = asyncio.get_event_loop()
+        loop.set_debug(True)
+        return self.loop.run_until_complete(self.server.get(key))
+
+    def set(self, key, value):
+        return self.loop.run_until_complete(self.server.set(key, value))
