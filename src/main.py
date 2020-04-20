a248754e92915d3cf34fb8194308db4a621dc369
Jackson Bursch <jacksonbursch@Jacksons-MacBook-Pro.local>
2020-04-19 18:52:13 -0700

    more main design features

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
 
+
 print("-----------------------------------------------------------------")
 print("**************************** Welcome ****************************")
 print("** This is Jackson and Brian's Peer to Peer Distrubted Network **")
@@ -7,11 +8,17 @@ print("********* Semester Lab Project for CECS 327 Spring 2020 *********")
 print("*****************************************************************")
 print("-----------------------------------------------------------------")
 
-#print(f"\n Do you want to join the network? (Yes/No)")
 ans = input("\nDo you want to join the network? (Yes/No) : ")
 
 if ans[0] == "Y" or ans[0] == "y":
 	#PUT CODE TO JOIN THE NETWORK HERE
+	print("\nExample for entering network - 0, 123.123.123, 4586")
+	nodeInput = input("\nPlease input your PeerID, IP Address, and Port : ")
+	[peerid, host, port] = nodeInput.split(",")
+	print(peerid,host,port)
+	#node.BTPeerConnection(peerid, host, port)
+	#import server
+	#server()
 	print("Success")
 else:
 	print("That's too bad! Maybe next time.")
